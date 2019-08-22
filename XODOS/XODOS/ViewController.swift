d56587a7da3225a02a8b1bce6ed24626af878933
TheMukh <themukh@yahoo.com>
2019-08-22 05:57:18 +0100

    added gradient effect on login screen

--- XODOS/XODOS/ViewController.swift
+++ XODOS/XODOS/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    
+    let gradientLayer = CAGradientLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundView.layer.addSublayer(gradientLayer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        landingBackground()
     }
 
+    func landingBackground(){
+        let topColour = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0, alpha: 1.0).cgColor
+        let bottomColour = UIColor(red:72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha:1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColour, bottomColour]
+    }
 
 }
 
