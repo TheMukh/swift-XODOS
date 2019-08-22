712c93e8035fc82ffa4f44396e343a160f551247
TheMukh <themukh@yahoo.com>
2019-08-22 05:30:36 +0100

    Initial push

---
+++ XODOS/XODOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XODOS
+//
+//  Created by IO on 22/08/2019.
+//  Copyright © 2019 mukh.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
